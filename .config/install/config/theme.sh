cc149d025b45f9438a00a8642659018ec623bcca
Beuk81 <157412048+Beuk81@users.noreply.github.com>
2025-11-22 22:16:37 +0100

    Update theme.sh

--- .config/install/config/theme.sh
+++ .config/install/config/theme.sh
@@ -4,10 +4,10 @@ ln -snf ~/.config/theme/tokyo-night ~/.config/current/theme
 ln -snf ~/.config/current/theme/backgrounds/1-scenery-pink-lakeside-sunset-lake-landscape-scenic-panorama-7680x3215-144.png ~/omarchy/current/background
 
 mkdir -p ~/.config/btop/themes
-ln -snf ~/.config/omarchy/current/theme/btop.theme ~/.config/btop/themes/current.theme
+ln -snf ~/.config/current/theme/btop.theme ~/.config/btop/themes/current.theme
 
 mkdir -p ~/.config/mako
-ln -snf ~/.config/omarchy/current/theme/mako.ini ~/.config/mako/config
+ln -snf ~/.config/current/theme/mako.ini ~/.config/mako/config
 
 # Add managed policy directorie for Chromium for theme changes
 sudo mkdir -p /etc/chromium/policies/managed
